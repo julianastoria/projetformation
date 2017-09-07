f608c27adb8ecff68663125bae35bc0edbf2d2bc
erwan lecrom <erwanlecrom@outlook.fr>
2017-09-07 10:40:51 +0200

    ajout route ajax

--- app/routes.php
+++ app/routes.php
@@ -37,8 +37,8 @@
 		['GET'		, '/etablissement/[i:id]/delete'	, 'Institutions#delete', 'institution_delete'],
 		//InstitutionNotes Routes
 		['GET|POST'	, '/etablissement/note/[i:id]/add', 'InstitutionNotes#create', 'create_institution_note'],
-		['GET[POST'	, '/etablissement/note/[i:id]/edit', 'InstitutionsNote#update', 'edit_institution_note'],
-		['GET'		, '/etablissement/note/[i:id]/delete', 'InstitutionsNote#delete', 'delete_institution_note'],
+		['GET[POST'	, '/etablissement/note/[i:id]/edit', 'InstitutionNote#update', 'edit_institution_note'],
+		['GET'		, '/etablissement/note/[i:id]/delete', 'InstitutionNote#delete', 'delete_institution_note'],
 
 		// AJAX - Département Route
 		['GET'		, '/ajax/departement', 'Doctors#ajaxDepartement', 'ajax_departement']
