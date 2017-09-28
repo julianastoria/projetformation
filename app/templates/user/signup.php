5df2e9a3155c3db7110318465e53e4bd3ad7d1fe
erwan lecrom <erwanlecrom@outlook.fr>
2017-09-28 10:10:09 +0200

    inscription accessibilite

--- app/templates/user/signup.php
+++ app/templates/user/signup.php
@@ -19,63 +19,87 @@
 					<label for="email">Identifiant (Adresse Mail) : </label>
 					<input type="email" name="email" class="form-control" id="email" value="<?= $email ?>"><br/>
 				</div>
+				<fieldset>
+					<legend class="hide_access">Mot de passe :</legend>
+					<div class="form-group">
+						<label for="password">Mot de passe :</label>
+						<input type="password" class="form-control" name="password" id="password"><br/>
+					</div>
 
-				<div class="form-group">
-					<label for="password">Mot de passe :</label>
-					<input type="password" class="form-control" name="password" id="password"><br/>
-				</div>
+					<div class="form-group">
+						<label for="repeat_password">Répétez le mot de passe :</label>
+						<input type="password" class="form-control" name="repeat_password" id="repeat_password"><br/>
+					</div>
+				</fieldset>
+				<fieldset>
+					<legend class="hide_access">Informations personnelles :</legend>
+					<div class="form-group">
+						<label for="firstname">Prénom :</label>
+						<input type="text" name="firstname" class="form-control" id="firstname" value="<?= $firstname ?>"><br/>
+					</div>
 
-				<div class="form-group">
-					<label for="repeat_password">Répétez le mot de passe :</label>
-					<input type="password" class="form-control" name="repeat_password" id="repeat_password"><br/>
-				</div>
-
-				<div class="form-group">
-					<label for="firstname">Prenom :</label>
-					<input type="text" name="firstname" class="form-control" id="firstname" value="<?= $firstname ?>"><br/>
-				</div>
-
-				<div class="form-group">
-					<label for="lastname">Nom :</label>
-					<input type="text" name="lastname" class="form-control" id="lastname" value="<?= $lastname ?>"><br/>
-				</div>
-
-				<div class="form-group">
-					<label for="birthday">Date de naissance : </label>
-				<input type="date" name="birthday" class="form-control" id="birthday" value="<?= $birthday ?>"><br/>
-				</div>
-
-				<div class="form-group">
-					<label for="id_departement">Departement</label>
-					<select class="form-control" name="id_departement" id="id_departement">
-					<?php foreach ($departements as $key => $departement): ?>
-
-						<option value="<?= $departement['id'] ?>"><?= $departement['name'] ?></option>
-					<?php endforeach ?>
-					</select><br/>
-				</div>
-
-				<div class="form-group">
-					<label for="id_autism">Autismes</label>
-					<select class="form-control" name="id_autism" id="id_autism">
-						<?php foreach ($autisms as $key => $autism): ?>
-
-						<option value="<?= $autism['id'] ?>"><?= $autism['name'] ?></option>
+					<div class="form-group">
+						<label for="lastname">Nom :</label>
+						<input type="text" name="lastname" class="form-control" id="lastname" value="<?= $lastname ?>"><br/>
+					</div>
+					<fieldset>
+						<legend>Date de naissance :</legend>
+						<div class="row">
+							<select class="col-md-3">
+								<?php for($i=1;$i<31;$i++) : ?>
+									<option value="<?= $i ?>"><?= $i ?></option>
+								<?php endfor ; ?>
+							</select>
+							<select class="col-md-3 col-md-offset-1">
+								<?php for($i=1;$i<13;$i++) : ?>
+									<option value="<?= $i ?>"><?= $i ?></option>
+								<?php endfor ; ?>
+							</select>
+							<select class="col-md-3 col-md-offset-1">
+								<?php for($i=1900;$i<=date(Y);$i++) : ?>
+									<option value="<?= $i ?>"><?= $i ?></option>
+								<?php endfor ; ?>
+							</select>
+						</div>
+					</fieldset>
+					<div class="form-group">
+						<label for="id_departement">Département</label>
+						<select class="form-control" name="id_departement" id="id_departement">
+						<?php foreach ($departements as $key => $departement): ?>
+							<?php $new_region=$departement['region'] ?>
+							<?php if ($new_region===$last_region)  : ?>
+								
+								<option value="<?= $departement['id'] ?>"><?= $departement['name'] ?></option>
+							<?php else :  ?>
+								<optgroup label="<?= $new_region ?>"><?= $new_region ?></optgroup>
+								<option value="<?= $departement['id'] ?>"><?= $departement['name'] ?></option>
+							<?php endif; ?>
+							<?php $last_region=$new_region; ?>
 						<?php endforeach ?>
-					</select><br/>						
-				</div>
+						</select><br/>
+					</div>
 
-				<div class="form-group">
-					<label for="situation">Situation</label>
-					<select class="form-control" name="situation" id="situation">
-						<option class="hidden" value="nothing">--</option>
-						<option value="autiste">Autiste</option>
-						<option value="parent d'autiste">Parent d'autiste</option>
-						<option value="medecin">Medecin</option>
-					</select><br/>
-				</div>
+					<div class="form-group">
+						<label for="id_autism">Autismes</label>
+						<select class="form-control" name="id_autism" id="id_autism">
+							<?php foreach ($autisms as $key => $autism): ?>
 
-				<button class="btn btn-purple" type="submit">S'inscrire</button>
+							<option value="<?= $autism['id'] ?>"><?= $autism['name'] ?></option>
+							<?php endforeach ?>
+						</select><br/>						
+					</div>
+
+					<div class="form-group">
+						<label for="situation">Situation</label>
+						<select class="form-control" name="situation" id="situation">
+							<option class="hidden" value="nothing">--</option>
+							<option value="autiste">Autiste</option>
+							<option value="parent d'autiste">Parent d'autiste</option>
+							<option value="medecin">Medecin</option>
+						</select><br/>
+					</div>
+				</fieldset>
+				<button class="btn btn-purple" type="submit">S'inscrire<span class='hide_access'>Renvoie vers la page de profil<span></button>
 			</form>
 
 		</div>
